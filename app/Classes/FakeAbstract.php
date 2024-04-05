1e710d28911e1da26fbef360f1654a73b6c246b3
edolbak <evd.kiev@gmail.com>
2024-04-05 13:42:08 +0300

    test abstract

---
+++ app/Classes/FakeAbstract.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Classes;
+
+abstract class FakeAbstract
+{
+    abstract public function func_abstract ();
+
+    public function func1 ()
+    {
+
+    }
+
+
+}
--- app/Http/Controllers/Fake/FakeUserController.php
+++ app/Http/Controllers/Fake/FakeUserController.php
@@ -20,6 +20,9 @@ class FakeUserController extends UserController
 
     public function test1()
     {
+
+
+
         // TODO-vardump VAR_DUMP
         die(var_dump('fake test1'));
 
